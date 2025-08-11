da6b3a23562331c17de9502d80c4d398af86058b
moritzgvt <hello@moritzgut.de>
2025-08-11 15:54:12 +0200

    Remove image module

--- modules/image/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  $layout = optionGet('layout');
-  $animation = optionGet('animation');
-?>
-
-<section class="grid module" <?php if ($animation) echo 'data-aos="fade-up"'; ?>>
-  <?php include 'layouts/image.php'; ?>
-</section>
--- modules/image/layouts/image.php
+++
@@ -1,5 +0,0 @@
-<div class="subgrid image end">
-  <div class="content--image col-w6p1">
-    <?php contentCreate( 1, 'image', 'autoxauto' ); ?>
-  </div>
-</div>
